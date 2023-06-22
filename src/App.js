423af5ca268d29a1a06e0d22716352e19ed68e88
itsAfnanAlam <afnanalam26@gmail.com>
2023-06-22 11:05:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./Components/Home/Home";
+import Header from "./Components/Header/Header";
+import About from "./Components/About/About";
+import Footer from "./Components/Footer/Footer";
+import Contact from "./Components/Contact/Contact";
+import Projects from "./Components/Projects/Projects";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="turn">
+        <img src="/img/rotatePhone.png" alt="" />
+        <h1>Please rotate your devices!</h1>
+      </div>
+      <div className="App" id="container">
+        <Header />
+        <Home />
+        <About />
+        <Projects />
+        <Contact />
+        <Footer />
+      </div>
+    </>
   );
 }
 
